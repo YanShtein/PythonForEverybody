3fc2754628b2a3dd5c09f49835ce26e65b0a4661
yansht <85824489+yansht@users.noreply.github.com>
2021-08-07 11:06:29 +0300

    Create Pay2_3.py

---
+++ Pay2_3.py
@@ -0,0 +1,4 @@
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+gross_pay = float(hrs) * float(rate)     #converts user input to float
+print("Pay:", gross_pay)                 #prints out the pay
