737a2b5f24700890260057feea61979b05765ef4
yansht <85824489+yansht@users.noreply.github.com>
2021-08-07 12:06:11 +0300

    Create extract_6_5.py

---
+++ extract_6_5.py
@@ -0,0 +1,4 @@
+text = 'X-DSPAM-Confidence:0.8475'
+colon_find = text.find(':')			#find the colon character
+number_find = text[colon_find+1:]	#add one to the colon position to extract from 0 to end of string from
+print(float(number_find))			#convert&print the extracted number to float
