e7d2d23a36e3965120c1b2d020658dcfe5e1ed2b
yansht <85824489+yansht@users.noreply.github.com>
2021-08-07 12:18:22 +0300

    Update 3.3.score_try.py

--- 3.3.score_try.py
+++ 3.3.score_try.py
@@ -16,3 +16,32 @@ try:
         print('bad score')
 except:
     print('Error...please enter a number')
+    
+--------
+#Below is the same code with def function added.
+--------
+
+score = input("Enter Score: ")
+
+
+def computegrade(grade):
+    try:
+        grade = float(score)
+        if 1.0 >= grade >= 0.9:
+            return "A"
+        elif 0.9 > grade >= 0.8:
+            return "B"
+        elif 0.8 > grade >= 0.7:
+            return "C"
+        elif 0.7 > grade >= 0.6:
+            return "D"
+        elif 0.0 < grade < 0.6:
+            return "F"
+        else:
+            return "Bad Score"
+    except:
+        return 'Error...please enter a number'
+
+
+print(computegrade(score))
+
