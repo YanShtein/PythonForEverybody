e2134aa761a6b8829644fa80e53987fbc363ca0e
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2021-08-09 14:33:52 +0300

    Update 7.1.open_file_upper.py

--- 7.1.open_file_upper.py
+++ 7.1.open_file_upper.py
@@ -1,3 +1,6 @@
+# Exercise 1: Write a program to read through a file and print the contents
+# of the file (line by line) all in upper case.
+
 file = open('mbox-short.txt.txt')
 # for each line in file, print in upper case with whitespace removed.
 for line in file:
