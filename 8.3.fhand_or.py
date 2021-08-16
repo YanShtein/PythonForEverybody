5868770b9b88965c31f23e3a3757412cd75a0bfb
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2021-08-16 09:06:49 +0300

    Update 8.3.fhand_or.py

--- 8.3.fhand_or.py
+++ 8.3.fhand_or.py
@@ -1,8 +1,8 @@
 # Use a compound logical expression using the or logical operator with a single if statement.
 
-fhand = open('mbox.txt.txt')
+fhand = open('test.txt')
 for line in fhand:
     words = line.split()
-    if len(words) == 0 or words[0] != 'From':
+    if len(words) == 0 or words[0] != 'From:':
         continue
-    print('final:', words[2])
+    print('final:', words[0])
