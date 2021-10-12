06352188db2f7cc813970b5253ca290a300b3f33
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2021-10-12 10:50:09 +0300

    Create 015.1.sqlite.py

---
+++ 015.1.sqlite.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn = sqlite3.connect('mbox.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+file = input('Enter file name: ')
+if len(file) < 1:
+    file = 'mbox.txt'
+file_open = open(file)
+for line in file_open:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    domain = pieces[1].split('@')[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (domain,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (domain,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (domain,))
+    conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC'
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
